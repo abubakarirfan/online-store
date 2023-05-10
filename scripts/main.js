db82c1685242ff9a1ae67c8a6043d540626d60b0
Uzman Shafi <uzmanshafifassy@gmail.com>
2023-05-10 14:44:39 +1000

    signup option in login

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 function goBack() {
+  const referrer = document.referrer;
+  const currentDomain = window.location.origin;
+
+  if (referrer && referrer.startsWith(currentDomain)) {
     window.history.back();
+  } else {
+    window.location.href = "../index.html"; // Replace with the default page URL within your site
   }
+}
